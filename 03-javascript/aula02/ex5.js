d4a508e1840f319d986890740b485e09e421ba15
momo-dev2 <samuellpasa@gmail.com>
2026-03-16 19:37:58 -0300

    exercicio 5

--- 03-javascript/aula02/ex5.js
+++ 03-javascript/aula02/ex5.js
@@ -1,1 +1,22 @@
 console.log("Exercício 5")
+
+
+//let valorRadar = number(prompt("Informe a velocidade registrada: "))
+let valorRadar = prompt("Informe a velocidade registrada: ")
+let valorRadarNum = Number(valorRadar)
+const limiteVelocidade = 50
+const multiplicadorMulta = 6
+
+if (valorRadarNum > limiteVelocidade) {
+    let valorMulta = (valorRadarNum - limiteVelocidade) * multiplicadorMulta
+    console.log(`Você foi multado! O valor da multa é R$${valorMulta}`)
+
+    alert(`MULTADO! Vc estava a ${valorRadar}Km/h e o limite neste trecho é ${limiteVelocidade} Km/h;
+Você deve pagar uma multa no valor de R$${valorMulta} pelo limite excedido.`)
+
+} else {
+    console.log("Velocidade dentro do limite.")
+
+    alert(`Velocidade dentro do limite. Você estava a ${valorRadar}Km/h e o limite neste trecho é ${limiteVelocidade} Km/h.`)
+    alert(`Velocidade ${valorRadar}Km/h, Dentro do limite. Tenha uma boa viagem!`)
+}
